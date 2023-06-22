bdc7bb0f97f882ddf9d5a638e4e5e775bc034f47
khaled85abo <khaledabou1985@gmail.com>
2023-06-23 00:00:11 +0200

    Add fetch and axios endpoints

--- app.js
+++ app.js
@@ -1,26 +1,83 @@
 const express = require("express");
 const app = express();
-
+const axios = require("axios");
+const fetch = require("node-fetch");
 app.use(express.static("public"));
 app.use(express.json());
 
+// let BASE_URL = "elasticsearch-oss:9200/sharepoint";
+let BASE_URL = "https://swapi.dev/api/";
+axios.BASE_URL = BASE_URL;
 const PORT = 3030;
 
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.status(200).send("Server is running");
 });
 
-app.post("/health", (req, res) => {
+app.post("/api/health", (req, res) => {
   const body = req.body;
   res.status(200).send(body);
 });
-app.get("/elastic", (req, res) => {
+app.get("/api/elastic", (req, res) => {
   res.status(200).send("Elastic is running");
 });
-app.post("/elastic", (req, res) => {
+app.post("/api/elastic", (req, res) => {
   const body = req.body;
   res.status(200).send(body);
 });
+app.get("/api/swapi", async (req, res) => {
+  try {
+    // Make the API request using Axios
+    const response = await axios.get(`${BASE_URL}people`);
+
+    // Handle the response
+    res.json(response.data);
+  } catch (error) {
+    // Handle any errors that occur during the request
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
+app.post("/api/customURL/fetch", async (req, res) => {
+  const url = req.body.url;
+  const query = req.body.query;
+  console.log(query);
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(query),
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
+app.post("/api/customURL/axios", async (req, res) => {
+  const url = req.body.url;
+  const query = req.body.query;
+  console.log(query);
+  try {
+    const response = await axios.post(url, query);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
+app.post("/api/customURL", async (req, res) => {
+  const url = req.body.url;
+  try {
+    const response = await axios.get(url);
+    // Handle the response
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
